d76a7749bd8b7ff895705e976dc7cd7ad3ec8131
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-06-01 11:47:43 +0200

    Proxy is now resolved correct

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+OPTIONS=""
+
 echo "Checking for user supplied certificates"
 if [ -n "$(ls -A /docker/custom-certs 2>/dev/null)" ]; then
     echo "Found user supplied certificates"
@@ -10,21 +12,23 @@ if [ -n "$(ls -A /docker/custom-certs 2>/dev/null)" ]; then
     update-ca-certificates || (echo -e "\nThe system has REJECTED one of the certificates:"; ls -l /custom-certs/*; echo "Make sure that ALL of the certificates are valid."; exit 1)
     echo "Successfully imported custom-certs."
 
-    OPTIONS += "tls-cafile=/usr/local/share/ca-certificates/"
+    OPTIONS+="tls-cafile=/usr/local/share/ca-certificates/ "
 
 fi
 
+
+if [ ! -z $PROXY_USERNAME ] || [ ! -z $PROXY_PASSWORD ]; then
+
+    OPTIONS+="login=${PROXY_USERNAME}:${PROXY_PASSWORD} "
+fi
 
 if [ ! -z $http_proxy ]; then
 
+PROXY_HOST=$(echo $http_proxy | awk -F "[/:]" '{print $4}' )
+PROXY_PORT=$(echo $http_proxy | awk -F "[/:]" '{print $5}' )
 
-    OPTIONS += " login=${PROXY_USERNAME}:${PROXY_PASSWORD}"
-fi
-
-if [ $http_proxy ]
-
-echo "parent ${PROXY_HOST}  ${PROXY_PORT}  0 ${OPTIONS} tls-cafile=/usr/local/share/ca-certificates/" >>  /etc/squid/squid.conf
+echo "cache_peer parent ${PROXY_HOST} ${PROXY_PORT} 0 ${OPTIONS}" ###>>  /etc/squid/squid.conf
 
 fi
 
-/usr/local/bin/entrypoint.sh -f /etc/squid/squid.conf -NYC
+##/usr/local/bin/entrypoint.sh -f /etc/squid/squid.conf -NYC
