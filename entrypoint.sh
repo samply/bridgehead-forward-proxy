931123ac0dbae4a043a32af38c7f6ba3385a7301
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-06-02 09:01:30 +0200

    Added User and Password extraction from http_proxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,27 +8,33 @@ if [ -n "$(ls -A /docker/custom-certs 2>/dev/null)" ]; then
     for file in /docker/custom-certs/*; do
         echo "Importing certificate $file to /usr/local/share/ca-certificates/$(basename $file).crt"
         cp -v $file /usr/local/share/ca-certificates/$(basename $file).crt
+        OPTIONS+="tls-cafile=/usr/local/share/ca-certificates/$(basename $file).crt"
     done
     update-ca-certificates || (echo -e "\nThe system has REJECTED one of the certificates:"; ls -l /custom-certs/*; echo "Make sure that ALL of the certificates are valid."; exit 1)
     echo "Successfully imported custom-certs."
-
-    OPTIONS+="tls-cafile=/usr/local/share/ca-certificates/ "
-
-fi
-
-
-if [ ! -z $PROXY_USERNAME ] || [ ! -z $PROXY_PASSWORD ]; then
-
-    OPTIONS+="login=${PROXY_USERNAME}:${PROXY_PASSWORD} "
 fi
 
 if [ ! -z $http_proxy ]; then
 
-PROXY_HOST=$(echo $http_proxy | awk -F "[/:]" '{print $4}' )
-PROXY_PORT=$(echo $http_proxy | awk -F "[/:]" '{print $5}' )
+    ## All credit to https://stackoverflow.com/a/6174447
+    # extract the protocol
+    PROTO="$(echo $http_proxy | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    # remove the protocol
+    URL="$(echo ${http_proxy/$PROTO/})"
+    # extract the user (if any)
+    USERPW="$(echo $URL | grep @ | cut -d@ -f1)"
+    # extract the host and port
+    HOSTPORT="$(echo ${URL/$USERPW@/} | cut -d/ -f1)"
+    # by request host without port    
+    HOST="$(echo $HOSTPORT | sed -e 's,:.*,,g')"
+    # by request - try to extract the port
+    PORT="$(echo $HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
 
-echo "cache_peer parent ${PROXY_HOST} ${PROXY_PORT} 0 ${OPTIONS}" >>  /etc/squid/squid.conf
+    if [ ! -z $USERPW ]; then
+        OPTIONS+="login=${USERPW} "
+    fi
 
+    echo "cache_peer parent ${HOST} ${PORT} 0 ${OPTIONS}" >>  /etc/squid/squid.conf
 fi
 
 /usr/local/bin/entrypoint.sh -f /etc/squid/squid.conf -NYC
