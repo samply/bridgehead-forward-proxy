ce27d0ce2b41cb45d08e385526232cf4e45540af
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2022-10-11 14:08:36 +0200

    Apply proxy fix

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ ADD ./entrypoint.sh ./proxify.sh /docker/
 RUN apt-get update -y && apt-get install -y \
     proxychains4 \
     && rm -rf /var/lib/apt/lists/*
-RUN sed -i 's/^# localnet /localnet /;s/^socks.*$/http PROXYIP PROXYPORT/' /etc/proxychains4.conf
+RUN sed -i 's/^# localnet /localnet /;s/^socks.*$/#http PROXYIP PROXYPORT/' /etc/proxychains4.conf
 RUN chmod +x /docker/entrypoint.sh /docker/proxify.sh
 ENTRYPOINT ["/docker/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,40 +14,53 @@ if [ -n "$(ls -A /docker/custom-certs 2>/dev/null)" ]; then
     echo "Successfully imported custom-certs."
 fi
 
-if [ ! -z $http_proxy ]; then
+: ${https_proxy:=$HTTPS_PROXY}
+: ${https_proxy:=$HTTP_PROXY}
+: ${https_proxy:=$http_proxy}
 
+if [ ! -z $https_proxy ]; then
+    echo "Configuring proxy $https_proxy"
     ## All credit to https://stackoverflow.com/a/6174447
     # extract the protocol
-    PROTO="$(echo $http_proxy | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    PROTO="$(echo $https_proxy | grep :// | sed -e's,^\(.*://\).*,\1,g')"
     # remove the protocol
-    URL="$(echo ${http_proxy/$PROTO/})"
+    URL="$(echo ${https_proxy/$PROTO/})"
     # extract the user and password (if any)
     USERPW="$(echo $URL | grep @ | cut -d@ -f1)"
     # extract the user
-    USER="$(echo $USERPW | cut -d: -f1)"
+    PROXY_USERNAME="$(echo $USERPW | cut -d: -f1)"
     # extract the password
-    PASSWORD="$(echo $USERPW | cut -d: -f2)"
+    PROXY_PASSWORD="$(echo $USERPW | cut -d: -f2)"
     # extract the host and port
     HOSTPORT="$(echo ${URL/$USERPW@/} | cut -d/ -f1)"
-    # by request host without port    
+    # by request host without port
     HOST="$(echo $HOSTPORT | sed -e 's,:.*,,g')"
     # by request - try to extract the port
     PORT="$(echo $HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
 
+    # If there was no user/password in the URL, check env vars USERNAME, PASSWORD
+    : ${PROXY_USERNAME:=$USERNAME}
+    : ${PROXY_PASSWORD:=$PASSWORD}
+
     IP="$(getent hosts $HOST | cut -d ' ' -f 1 | tail -1)"
     echo $IP
-    sed -e "s/PROXYIP/$IP/g; s/PROXYPORT/$PORT/g" /etc/proxychains4.conf > /tmp/proxychains4.conf
+    LINE="http $IP $PORT"
 
-    if [ ! -z $USERPW ]; then
+    if [ ! -z $PROXY_PASSWORD ]; then
         echo "User and Password detected"
-        sed -e "115s/$/ ${USER} ${PASSWORD}/" /tmp/proxychains4.conf > /tmp/proxychains4.conf
+        LINE+=" $PROXY_USERNAME $PROXY_PASSWORD"
     fi
 
+    cat /etc/proxychains4.conf > /tmp/proxychains4.conf
+    echo "$LINE" >> /tmp/proxychains4.conf
+
+    cat /tmp/proxychains4.conf
+
     if [ "proxychains-is-happy" != "$(/docker/proxify.sh echo proxychains-is-happy)" ]; then
-        echo "Error: Failed to configure proxychains with proxy $http_proxy (= http_proxy)"
+        echo "Error: Failed to configure proxychains with proxy $https_proxy (= https_proxy)"
         exit 1
     fi
 
 fi
 
-/docker/proxify.sh /usr/local/bin/entrypoint.sh -f /etc/squid/squid.conf -NYC
+exec /docker/proxify.sh /usr/local/bin/entrypoint.sh -f /etc/squid/squid.conf -NYC
