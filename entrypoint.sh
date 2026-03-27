a5e138b5030d69c34607de923cfaefcf66f156fc
Martin Lablans <m.lablans@dkfz-heidelberg.de>
2026-03-27 12:00:17 +0100

    Remove certificate handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,6 @@
 ## All credit to https://stackoverflow.com/questions/6250698/how-to-decode-url-encoded-string-in-shell
 function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 
-OPTIONS=""
-
-echo "Checking for user supplied certificates"
-if [ -n "$(ls -A /docker/custom-certs/*.pem 2>/dev/null)" ]; then
-    echo "Found user supplied certificates"
-    for file in /docker/custom-certs/*.pem; do
-        echo "Importing certificate $file to /usr/local/share/ca-certificates/$(basename $file).crt"
-        cp -v $file /usr/local/share/ca-certificates/$(basename $file).crt
-        OPTIONS+="tls-cafile=/usr/local/share/ca-certificates/$(basename $file).crt"
-    done
-    update-ca-certificates || (echo -e "\nThe system has REJECTED one of the certificates:"; ls -l /custom-certs/*; echo "Make sure that ALL of the certificates are valid."; exit 1)
-    echo "Successfully imported custom-certs."
-fi
-
 : ${https_proxy:=$HTTPS_PROXY}
 : ${https_proxy:=$HTTP_PROXY}
 : ${https_proxy:=$http_proxy}
